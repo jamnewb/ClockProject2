583c327268acc4911ab261e81295723fca4899e6
lljamesnll <funnyoohoohaahaah@gmail.com>
2024-09-18 13:42:08 +1000

    Add project files.

---
+++ Clock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClockProject2
+{
+    public class Clock
+    {
+        private Counter seconds;
+        private Counter minutes;
+        private Counter hours;
+        public Clock()
+        {
+            seconds = new Counter("seconds");
+            minutes = new Counter("minutes");
+            hours = new Counter("hours");
+        }
+        public void Tick()
+        {
+            if (seconds.Ticks != 59)
+            {
+                seconds.Increment();
+            }
+            else if (minutes.Ticks != 59 && seconds.Ticks == 59)
+            {
+                minutes.Increment();
+                seconds.Reset();
+            }
+            else
+            {
+                hours.Increment();
+                minutes.Reset();
+                seconds.Reset();
+            }
+
+            if (hours.Ticks == 24)
+            {
+                hours.Reset();
+                minutes.Reset();
+                seconds.Reset();
+            }
+        }
+        public string Read()
+        {
+            return $"{hours.Ticks:D2}:{minutes.Ticks:D2}:{seconds.Ticks:D2}";
+        }
+        public void Reset()
+        {
+            seconds.Reset();
+            minutes.Reset();
+            hours.Reset();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ClockProject2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace CounterTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+            int i = 0;
+
+            while (i <= 86402)
+            {
+                Console.WriteLine(myClock.Read());
+                myClock.Tick();
+                i++;
+            }
+        }
+    }
+}
